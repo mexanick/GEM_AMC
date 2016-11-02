bd978f33e81663cf39983d1074fdd6b5e346da9d
Evaldas Juska <evka85@gmail.com>
2016-11-02 02:26:43 +0100

    removed some unused files

--- ctp7/external/SOL40_SCA_new/SOL40_SCA_new.srcs/sources_1/imports/SOL40_SCA_sources/External_Packages/ext_pack.vhd
+++
@@ -1,9 +0,0 @@
-package Constant_Declaration is
-
-	constant ECS_address_size	:	integer:=19;
-	constant ECS_data_size		:	integer:=32;
-end package Constant_Declaration;
-
-package body Constant_Declaration is
-	
-end package body Constant_Declaration;
